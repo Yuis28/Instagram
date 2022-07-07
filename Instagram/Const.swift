cb8b1abf14a75275539730aedc313de89d048ffa
yui.s <y.u.i020928@gmail.com>
2022-07-07 14:10:32 +0900

    インスタグラムの基本の機能を搭載したアプリの完成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 杉田優衣 on 2022/07/06.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
